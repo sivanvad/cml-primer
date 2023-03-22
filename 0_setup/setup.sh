694ae7cc916b64e195edf0386c49bbbbe89666ba
Sivanesan Vadivel <svadivel@cloudera.com>
2023-03-22 09:25:15 +0000

    Data Exploration

--- 0_setup/setup.sh
+++ 0_setup/setup.sh
@@ -1,11 +1,11 @@
 # Download dataset from Kaggle
-kaggle competitions download -c amex-default-prediction
+# kaggle competitions download -c amex-default-prediction
 
 # Unzip data to local
-unzip amex-default-prediction.zip -d amex-default-prediction/
+# unzip amex-default-prediction.zip -d amex-default-prediction/
 
 # Set STORAGE environment
 python setup.py
 
 # Copy data to s3
-hdfs dfs -cp amex-default-prediction/* ${STORAGE}/ps-ml/american-express-kaggle/data/
+# hdfs dfs -cp amex-default-prediction/* ${STORAGE}/ps-ml/american-express-kaggle/data/
